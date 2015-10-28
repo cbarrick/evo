4255cf742663d29b3a5508527101e5647a36b10f
Chris Barrick <cbarrick1@gmail.com>
2015-10-28 15:35:00 -0400

    Allow chaining vector operations

--- real/vector.go
+++ real/vector.go
@@ -7,7 +7,7 @@ import (
 type Vector []float64
 
 // Random generates a random vector of length n. Values are taken uniformly
-// between [0,1) and multiplied by some scale.
+// between [0,scale).
 func Random(n int, scale float64) (v Vector) {
 	v = make(Vector, n)
 	for i := range v {
@@ -22,36 +22,41 @@ func (v Vector) Copy() Vector {
 	return w
 }
 
-func (v Vector) Add(w Vector) {
+func (v Vector) Add(w Vector) Vector {
 	for i := range v {
 		v[i] += w[i]
 	}
+	return v
 }
 
-func (v Vector) Subtract(w Vector) {
+func (v Vector) Subtract(w Vector) Vector {
 	for i := range v {
 		v[i] -= w[i]
 	}
+	return v
 }
 
-func (v Vector) Scale(s float64) {
+func (v Vector) Scale(s float64) Vector {
 	for i := range v {
 		v[i] *= s
 	}
+	return v
 }
 
-func (v Vector) LowBound(min float64) {
+func (v Vector) LowBound(min float64) Vector {
 	for i := range v {
 		if v[i] < min {
 			v[i] = min
 		}
 	}
+	return v
 }
 
-func (v Vector) HighBound(max float64) {
+func (v Vector) HighBound(max float64) Vector {
 	for i := range v {
 		if v[i] > max {
 			v[i] = max
 		}
 	}
+	return v
 }
